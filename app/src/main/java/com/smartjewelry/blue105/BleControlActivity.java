c8e4282249f781c4bf5f56acbe2ab27fcde39692
qicaimantou <345574438@qq.com>
2017-08-28 15:42:27 +0800

    update firmware

--- app/src/main/java/com/smartjewelry/blue105/BleControlActivity.java
+++ app/src/main/java/com/smartjewelry/blue105/BleControlActivity.java
@@ -127,7 +127,7 @@ public class BleControlActivity extends AppCompatActivity implements View.OnClic
             BluetoothDevice device = mBleService.gatt.getDevice();
             if (device != null) {
                 DfuServiceInitiator starter = new DfuServiceInitiator(device.getAddress()).setDeviceName(device.getName()).setKeepBond(false);
-                starter.setZip(R.raw.v105_212_20170825);
+                starter.setZip(R.raw.v105_213_20170828);
                 starter.start(this, BleDfuService.class);
             } else {
                 Log.e(TAG, "device = null");
